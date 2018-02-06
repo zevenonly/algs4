ce1af96a13237cd3a0b8ca5528136992879c2d4f
zeven <351064265@qq.com>
2018-02-06 20:25:59 +0800

    递交希尔排序

---
+++ src/main/java/practice/week02/ShellSort.java
@@ -0,0 +1,17 @@
+package practice.week02;
+
+public class ShellSort {
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        int h = N / 3 + 1;
+        for (int i = h; i < N; i++) {
+            for (int j = i - h; j >= 0; j -= h) {
+                if (SortUtil.less(a[j], a[j - h])) {
+                    SortUtil.exch(a, j, j - h);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+}
