510cf175047dcc3257a224ddf2851d3f20a9e621
zeven <351064265@qq.com>
2018-02-06 20:27:25 +0800

    递交希尔排序

--- src/main/java/practice/week02/ShellSort.java
+++ src/main/java/practice/week02/ShellSort.java
@@ -4,14 +4,17 @@ public class ShellSort {
     public static void sort(Comparable[] a) {
         int N = a.length;
         int h = N / 3 + 1;
-        for (int i = h; i < N; i++) {
-            for (int j = i - h; j >= 0; j -= h) {
-                if (SortUtil.less(a[j], a[j - h])) {
-                    SortUtil.exch(a, j, j - h);
-                } else {
-                    break;
+        while (h >= 1) {
+            for (int i = h; i < N; i++) {
+                for (int j = i - h; j >= 0; j -= h) {
+                    if (SortUtil.less(a[j], a[j - h])) {
+                        SortUtil.exch(a, j, j - h);
+                    } else {
+                        break;
+                    }
                 }
             }
+            h = h / 3;
         }
     }
 }
